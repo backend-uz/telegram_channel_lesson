614dc7c6415c5948615c1dc7756bc7e9eb3b11de
user1 <backenduz@gmail.com>
2023-01-10 16:06:43 +0500

    number of posts

--- get_number_of_posts.py
+++ get_number_of_posts.py
@@ -10,4 +10,7 @@ def get_number_of_posts(data:dict)->int:
     Returns: 
         int: the number of posts for the given dictionary
     """
-    return
+
+    return len(data['messages'])
+data = fromJson('data/result.json')
+print(get_number_of_posts(data))
